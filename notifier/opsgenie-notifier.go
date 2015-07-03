c60b0fa3a70262db8bb2ae34aa7ad55b1daa5391
Bastien Dronneau <basti1.dr@gmail.com>
2015-07-03 17:13:44 +0200

    For each message received we sent opsgenie notification

--- notifier/opsgenie-notifier.go
+++ notifier/opsgenie-notifier.go
@@ -14,41 +14,47 @@ type OpsGenieNotifier struct {
 	ApiKey   string
 }
 
-func (notifier *OpsGenieNotifier) Notify(messages Messages) bool {
+func (opsgenie *OpsGenieNotifier) Notify(messages Messages) bool {
 
 	overallStatus, pass, warn, fail := messages.Summary()
 	var title string
-	text := fmt.Sprintf(header, notifier.ClusterName, overallStatus, fail, warn, pass)
-
-	for _, message := range messages {
-		text += fmt.Sprintf("\n%s:%s:%s is %s.", message.Node, message.Service, message.Check, message.Status)
-		text += fmt.Sprintf("\n%s", message.Output)
-		title += fmt.Sprintf("\n%s:%s:%s is %s.", message.Node, message.Service, message.Check, message.Status)
-	}
+	content := fmt.Sprintf(header, opsgenie.ClusterName, overallStatus, fail, warn, pass)
 
 	client := new(ogcli.OpsGenieClient)
-	client.SetApiKey(notifier.ApiKey)
+	client.SetApiKey(opsgenie.ApiKey)
 
 	alertCli, cliErr := client.Alert()
 
 	if cliErr != nil {
-                log.Println("Opsgenie notification trouble with client")
+		log.Println("Opsgenie notification trouble with client")
 		return false
 	}
 
-	// create the alert
-	req := alerts.CreateAlertRequest{
-		Message:      title,
-		Description:  text,
-		Source:       "consul",
-	}
-	response, alertErr := alertCli.Create(req)
+	for _, message := range messages {
+		title += fmt.Sprintf("\n%s:%s:%s is %s.", message.Node, message.Service, message.Check, message.Status)
+		content += fmt.Sprintf("\n%s:%s:%s is %s.", message.Node, message.Service, message.Check, message.Status)
+		content += fmt.Sprintf("\n%s", message.Output)
 
-	if alertErr != nil {
-        log.Println("Opsgenie notification trouble.", response.Status)
-		return false
+		// create the alert
+		response, alertErr := opsgenie.Send(alertCli, title, content)
+
+		if alertErr != nil {
+			log.Println("Opsgenie notification trouble.", response.Status)
+			return false
+		}
+
 	}
 
-        log.Println("Opsgenie notification send.")
+	log.Println("Opsgenie notification send.")
 	return true
+}
+
+func (opsgenie *OpsGenieNotifier) Send(alertCli *ogcli.OpsGenieAlertClient, message string, content string) (*alerts.CreateAlertResponse, error) {
+	req := alerts.CreateAlertRequest{
+		Message:         message,
+		Description:     content,
+		Source:          "consul",
+		Entity:          opsgenie.ClusterName,
+	}
+	return alertCli.Create(req)
 }
